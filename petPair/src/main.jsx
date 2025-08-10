91a2beb47ce74bcde83db56efc2e1414bd40295c
Liamwu00 <at252867349@gmail.com>
2025-08-10 00:36:06 -0700

    finish display card page

--- petPair/src/main.jsx
+++ petPair/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import WelcomePage from './routes/WelcomePage.jsx'
 import Layout from './routes/Layout.jsx'
 // import App from './App.jsx'
 import RabbitDisplayPage from './routes/RabbitDisplayPage.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- petPair/src/routes/RabbitDisplayPage.jsx
+++ petPair/src/routes/RabbitDisplayPage.jsx
@@ -2,22 +2,50 @@ import RabbitCard from "../components/rabbitDisplay/RabbitCard";
 import rabbitData from '../rabbits.json'
 import React, { useState, useEffect } from 'react';
 import './RabbitDisplayPage.css'
+import SearchIcon from '../assets/images/search.svg'
+import SortIcon from '../assets/images/filter-left.svg'
+
 
 const RabbitDisplayPage = ()=>{
     const [rabbits, setRabbits] = useState([]);
+    const [filteredNumbers, setFilteredNumbers]= useState(0);
     useEffect(()=>{
         setRabbits(rabbitData.rabbits)
+        setFilteredNumbers(rabbitData.rabbits.length)
     },[])
     console.log(rabbits)
     return (
-        <div className="display-body">
-            {
-                rabbits.map(rabbit=>(
-                    <RabbitCard key={rabbit.id} rabbit={rabbit}/>
-                ))
-            }
-            
+        <div className="display-page-container">  
+            {/* seacrh bar */}
+            <div className="search-bar">
+                <input type="text" placeholder="Search"/>
+                <button>
+                    <img src={SearchIcon} alt="" />
+                </button>
+            </div>
+
+            <div className="sort-part">
+                <div>
+                    <img src={SortIcon} alt="" className="sort-icon"/>
+                    <span>Sort by</span>
+                </div>
+
+                <p>{filteredNumbers} results</p>
+            </div>
+
+
+
+
+            <div className="display-body">
+                {
+                    rabbits.map(rabbit=>(
+                        <RabbitCard key={rabbit.id} rabbit={rabbit}/>
+                    ))
+                }
+                
+            </div>
         </div>
+
     )
 }
 
