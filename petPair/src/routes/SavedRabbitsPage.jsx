f03037203ede71b9f1b45eb0382afc4f68443aeb
Liamwu00 <at252867349@gmail.com>
2025-08-12 15:25:51 -0700

    change button on saved rabbit and shellter page

--- petPair/src/routes/SavedRabbitsPage.jsx
+++ petPair/src/routes/SavedRabbitsPage.jsx
@@ -3,7 +3,7 @@ import RabbitCard from "../components/rabbitDisplay/RabbitCard";
 import { Link } from "react-router-dom";
 import LeftArrowIcon from '../assets/images/arrow-left.svg'; // back arrow icon
 import './SavedRabbitsPage.css'; // includes backToDisplay styling
-
+import LeftArrow from '../components/LeftArrow'
 const SavedRabbitsPage = () => {
     const [savedRabbits, setSavedRabbits] = useState([]);
 
@@ -36,34 +36,49 @@ const SavedRabbitsPage = () => {
 
     return (
         <div className="display-page-container">
-            
+            <LeftArrow/>
+
+
+            <div className="saved-rabbits-containter">
+
+                {savedRabbits.length > 0 ? (
+                    <div className="display-body">
+                        {savedRabbits.map((rabbit) => (
+                            <Link
+                                to={`/display/detail/${rabbit.details_page.id}`}
+                                key={rabbit.details_page.id}
+                                className="rabbit-link"
+                            >
+                                <RabbitCard rabbit={rabbit} />
+                            </Link>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No saved rabbits yet.</p>
+                )}
+            </div>
+
+
+
+            <div className="buttons-container">
+
 
             {/* Clear all button */}
             {savedRabbits.length > 0 && (
-                <button
+                <Link to={'/display'} key={'gobacktodisplay'}>
+                    <button
                     className="clear-filters-button" 
                     style={{ marginBottom: "16px" }}
                     onClick={clearAllSaved}
-                >
+                    >
+
                     Clear All Saved
                 </button>
+                </Link>
+
             )}
 
-            {savedRabbits.length > 0 ? (
-                <div className="display-body">
-                    {savedRabbits.map((rabbit) => (
-                        <Link
-                            to={`/display/detail/${rabbit.details_page.id}`}
-                            key={rabbit.details_page.id}
-                            className="rabbit-link"
-                        >
-                            <RabbitCard rabbit={rabbit} />
-                        </Link>
-                    ))}
-                </div>
-            ) : (
-                <p>No saved rabbits yet.</p>
-            )}
+            </div>
         </div>
     );
 };
